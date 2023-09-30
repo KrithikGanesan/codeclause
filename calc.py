50536bac7c2b692f756f4b1ba3c80de482b852a4
KrithikGanesan <krithikganesan01@gmail.com>
2023-09-30 09:35:59 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,38 +1,74 @@
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y != 0:
-        return x / y
+capchar=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+dig = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+spchar=['!','@','#','$','%','^','&','*','(',')','_','-',',','.']
+smallchar=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'] 
+import random
+passlen=int(input("Enter the desired length of password : "))
+capans=str(input("Are CAPITAL Alphabets allowed ? (Y/N) : "))
+smallans=str(input("Are lowercase Alphabets allowed ? (Y/N) : "))
+digans=str(input("Are Digits allowed ? (Y/N) : "))
+spans=str(input("Are Special Charecters allowed ? (Y/N) : "))
+print("Way 1 is meant for Minimal Resource Usage and Way 2 for more secure generation.")
+rep=input("Way 1 or Way 2 ? (1/2) : ")
+capans,smallans,digans,spans=capans.title(),smallans.title(),digans.title(),spans.title()
+if capans=='Y':
+    if smallans=='Y':
+        if digans=='Y':
+            if spans=='Y':
+                    comblis=capchar+dig+spchar+smallchar
+            else:
+                    comblis=capchar+dig+smallchar
+        else :
+            if spans=='Y':
+                    comblis=capchar+spchar+smallchar
+            else:
+                    comblis=capchar+smallchar
     else:
-        return "Error: Division by zero"
+        if digans=='Y':
+            if spans=='Y':
+                    comblis=capchar+dig+spchar
+            else:
+                    comblis=capchar+dig
+        else :
+            if spans=='Y':
+                    comblis=capchar+spchar
+            else:
+                    comblis=capchar
+else :
+     if smallans=='Y':
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=dig+spchar+smallchar
+               else:
+                    comblis=dig+smallchar
+          else :
+               if spans=='Y':
+                    comblis=spchar+smallchar
+               else:
+                    comblis=smallchar
+     else:
+          if digans=='Y':
+               if spans=='Y':
+                    comblis=dig+spchar
+               else:
+                    comblis=dig
+          else :
+               if spans=='Y':
+                    comblis=ar+spchar
+               else:
+                    print("Error, You denied the usage of all possible charecters")
+                    quit()
 
-if __name__ == "__main__":
-    print("Select operation:")
-    print("1. Addition")
-    print("2. Subtraction")
-    print("3. Multiplication")
-    print("4. Division")
-
-    choice = input("Enter choice (1/2/3/4): ")
-
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-    else:
-        print("Invalid input")
+password=""
+if rep=='1':
+    for i in range(passlen):
+        password+=comblis[random.randint(0,len(comblis)-1)]
+        
+else:
+    random.shuffle(comblis)
+    for i in range(passlen):
+        q=random.choice(comblis)
+        random.shuffle(comblis)
+        password=password+q
+print("\nGenerated Password is : ",password)
+print()
