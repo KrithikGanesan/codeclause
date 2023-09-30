ba5de8d00b54feb7a829a2dbd04db7b639ce20a6
KrithikGanesan <krithikganesan01@gmail.com>
2023-09-30 21:15:03 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,74 +1,51 @@
-capchar=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-dig = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-spchar=['!','@','#','$','%','^','&','*','(',')','_','-',',','.']
-smallchar=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'm', 'n', 'o', 'p', 'q','r', 's', 't', 'u', 'v', 'w', 'x', 'y','z'] 
-import random
-passlen=int(input("Enter the desired length of password : "))
-capans=str(input("Are CAPITAL Alphabets allowed ? (Y/N) : "))
-smallans=str(input("Are lowercase Alphabets allowed ? (Y/N) : "))
-digans=str(input("Are Digits allowed ? (Y/N) : "))
-spans=str(input("Are Special Charecters allowed ? (Y/N) : "))
-print("Way 1 is meant for Minimal Resource Usage and Way 2 for more secure generation.")
-rep=input("Way 1 or Way 2 ? (1/2) : ")
-capans,smallans,digans,spans=capans.title(),smallans.title(),digans.title(),spans.title()
-if capans=='Y':
-    if smallans=='Y':
-        if digans=='Y':
-            if spans=='Y':
-                    comblis=capchar+dig+spchar+smallchar
-            else:
-                    comblis=capchar+dig+smallchar
-        else :
-            if spans=='Y':
-                    comblis=capchar+spchar+smallchar
-            else:
-                    comblis=capchar+smallchar
-    else:
-        if digans=='Y':
-            if spans=='Y':
-                    comblis=capchar+dig+spchar
-            else:
-                    comblis=capchar+dig
-        else :
-            if spans=='Y':
-                    comblis=capchar+spchar
-            else:
-                    comblis=capchar
-else :
-     if smallans=='Y':
-          if digans=='Y':
-               if spans=='Y':
-                    comblis=dig+spchar+smallchar
-               else:
-                    comblis=dig+smallchar
-          else :
-               if spans=='Y':
-                    comblis=spchar+smallchar
-               else:
-                    comblis=smallchar
-     else:
-          if digans=='Y':
-               if spans=='Y':
-                    comblis=dig+spchar
-               else:
-                    comblis=dig
-          else :
-               if spans=='Y':
-                    comblis=ar+spchar
-               else:
-                    print("Error, You denied the usage of all possible charecters")
-                    quit()
+from tkinter import *                                                  
 
-password=""
-if rep=='1':
-    for i in range(passlen):
-        password+=comblis[random.randint(0,len(comblis)-1)]
-        
-else:
-    random.shuffle(comblis)
-    for i in range(passlen):
-        q=random.choice(comblis)
-        random.shuffle(comblis)
-        password=password+q
-print("\nGenerated Password is : ",password)
-print()
+def btnClick(numbers):                                                  
+    global operator                                                     
+    operator = operator + str(numbers)
+    text_Input.set(operator)                                            
+    
+def btnClear():                                                         
+    global operator
+    operator=""
+    text_Input.set("")                                                  
+
+def btnEquals():
+    global operator
+    sumup=str(eval(operator))                                           
+    text_Input.set(sumup)                                               
+    operator=""                                                         
+    
+cal = Tk()                                                              
+cal.title("Calculator")                                                 
+operator=""
+text_Input= StringVar()
+
+
+#GUI for the calculator
+
+
+txtDisplay = Entry(cal, font=('arial', 20,"bold"), textvariable=text_Input, bd=30, insertwidth=4,bg="powder blue", justify="right").grid(columnspan=4)
+
+btn7=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="7", command=lambda:btnClick(7), bg="powder blue").grid(row=1,column=0)
+btn8=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="8", command=lambda:btnClick(8),bg="powder blue").grid(row=1,column=1)
+btn9=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="9",command=lambda:btnClick(9),bg="powder blue").grid(row=1,column=2)
+btn_add=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="+",command=lambda:btnClick("+"),bg="powder blue").grid(row=1,column=3)
+
+btn4=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="4",command=lambda:btnClick(4),bg="powder blue").grid(row=2,column=0)
+btn5=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="5",command=lambda:btnClick(5),bg="powder blue").grid(row=2,column=1)
+btn6=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="6",command=lambda:btnClick(6),bg="powder blue").grid(row=2,column=2)
+btn_times=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="*",command=lambda:btnClick("*"),bg="powder blue").grid(row=2,column=3)
+
+btn1=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="1",command=lambda:btnClick(1),bg="powder blue").grid(row=3,column=0)
+btn2=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="2",command=lambda:btnClick(2),bg="powder blue").grid(row=3,column=1)
+btn3=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="3",command=lambda:btnClick(3),bg="powder blue").grid(row=3,column=2)
+btn_minus=Button(cal,padx=16,bd=8, fg="black",font=('arial',20,'bold'),text="-",command=lambda:btnClick("-"),bg="powder blue").grid(row=3,column=3)
+
+btn0=Button(cal,padx=16,pady=16,bd=8, fg="black",font=('arial',20,'bold'),text="0",command=lambda:btnClick(0),bg="powder blue").grid(row=4,column=0)
+btn_clear=Button(cal,padx=16,pady=16,bd=8, fg="black",font=('arial',20,'bold'),text="C",bg="powder blue",command=btnClear).grid(row=4,column=1)
+btn_equals=Button(cal,padx=16,pady=16,bd=8, fg="black",font=('arial',20,'bold'),text="=",bg="powder blue",command=btnEquals).grid(row=4,column=2)
+btn_div=Button(cal,padx=16,pady=16,bd=8, fg="black",font=('arial',20,'bold'),text="/",command=lambda:btnClick("/"),bg="powder blue").grid(row=4,column=3)
+
+#=============================================================================================================================================================
+cal.mainloop()
